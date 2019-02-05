2dff52160786183852b900ba37d71c53bb0c8b3c
Nuno Farinha <nadfarinha@gmail.com>
2019-02-05 16:43:02 +0000

    Initial commit

---
+++ models/game.rb
@@ -0,0 +1,10 @@
+require "pry-byebug"
+class Game
+
+  def initialize(throw1, throw2)
+    @throw1 = throw1
+    @throw2 = throw2
+  end
+
+  #END OF CLASS
+end
---
+++ specs/game_spec.rb
@@ -0,0 +1,32 @@
+require "minitest/autorun"
+require "minitest/rg"
+require_relative "../models/game"
+
+
+class TestGame < Minitest::Test
+  def setup
+
+    @throw_1 = "rock"
+    @throw_2 = "paper"
+    @throw_3 = "scissors"
+
+#Paper wins rock
+    @game_1 = Game.new(@throw_1, @throw_2)
+    @game_2 = Game.new(@throw_2, @throw_1)
+
+#Rock wins scissors
+    @game_3 = Game.new(@throw_1, @throw_3)
+    @game_4 = Game.new(@throw_3, @throw_1)
+
+#Scissors wins paper
+    @game_5 = Game.new(@throw_2, @throw_3)
+    @game_6 = Game.new(@throw_3, @throw_2)
+
+#Ties
+    @game_7 = Game.new(@throw_1, @throw_1)
+    @game_8 = Game.new(@throw_2, @throw_2)
+    @game_9 = Game.new(@throw_3, @throw_3)
+
+  end
+
+end
