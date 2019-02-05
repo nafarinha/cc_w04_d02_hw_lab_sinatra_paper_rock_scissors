ecbf28348541a66e36fe58ad0f7a4b18f1ce8634
Nuno Farinha <nadfarinha@gmail.com>
2019-02-05 16:47:57 +0000

    Create game logic class

--- models/game.rb
+++ models/game.rb
@@ -6,5 +6,37 @@ class Game
     @throw2 = throw2
   end
 
+  # def play
+  #   rock_wins = "Rock wins"
+  #   paper_wins = "Paper wins"
+  #   scissors_wins = "Scissors wins"
+  #
+  #   if @throw1 == "rock" && @throw2 == "paper"
+  #     return paper_wins
+  #   elsif @throw1 == "paper" && @throw2 == "rock"
+  #     return paper_wins
+  #   elsif @throw1 == "rock" && @throw2 == "scissors"
+  #     return rock_wins
+  #   elsif @throw1 == "scissors" && @throw2 == "rock"
+  #     return rock_wins
+  #   elsif @throw1 == "paper" && @throw2 == "scissors"
+  #     return scissors_wins
+  #   elsif @throw1 == "scissors" && @throw2 == "paper"
+  #     return scissors_wins
+  #   else
+  #     return "Tie!"
+  #   end
+  # end
+
+  def play
+    throws = []
+    throws.push(@throw1, @throw2)
+
+    return "Paper wins" if throws.include?("paper") && throws.include?("rock")
+    return "Rock wins" if throws.include?("rock") && throws.include?("scissors")
+    return "Scissors wins" if throws.include?("scissors") && throws.include?("paper")
+    return "Tie!"
+  end
+
   #END OF CLASS
 end
