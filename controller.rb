e9a8ff8c59968bd31cff7722c3057797c59d521b
Nuno Farinha <nadfarinha@gmail.com>
2019-02-05 16:50:22 +0000

    Create the game controller

--- controller.rb
+++ controller.rb
@@ -2,3 +2,10 @@ require "sinatra"
 require "sinatra/contrib/all" if development?
 require "pry-byebug"
 require_relative "./models/game"
+
+
+get '/play/:throw1/:throw2' do
+  game = Game.new(params[:throw1], params[:throw2])
+  return game.play
+
+end
